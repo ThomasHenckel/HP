f681fad492c5c4aeb879a798f45dacfeaa2350d8
unknown <the@T430SW7THE.nclan.netcompany.dk>
2013-05-26 10:56:22 +0200

    My first Ruby on Rails Project

---
+++ HistoPath/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HistoPath::Application.config.secret_key_base = '91f4723081bda75c14187675b64d752348feff9dfab9b6a2400cbd7a0fba9a9a2d320a68c708e1445ff9bec41339cddf257420f3d8f92965a8cdb6643b1af687'
